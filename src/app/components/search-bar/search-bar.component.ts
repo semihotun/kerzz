139729fbc44ac914b22acd3e2b15c01c4e633b85
semih <semihotun1@gmail.com>
2024-04-29 06:48:53 +0300

    Add Search Bar

---
+++ src/app/components/search-bar/search-bar.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { IonicModule } from '@ionic/angular';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss'],
+  standalone: true,
+  imports: [CommonModule, IonicModule]
+})
+export class SearchBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() { }
+
+}
